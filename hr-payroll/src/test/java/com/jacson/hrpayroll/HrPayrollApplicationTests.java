8ae520da95a993619a99213a01a5846f85523646
jacsonpalves <jacsonp.alves@gmail.com>
2020-11-25 13:32:19 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/jacson/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacson.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
