74c6753ba699456bca468baf237f986c27c1de8f
jacsonpalves <jacsonp.alves@gmail.com>
2020-12-13 22:15:02 -0300

    Add Project Oauth

---
+++ hr-user/src/main/java/com/jacson/hruser/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+
+package com.jacson.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jacson.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String email);
+}
---
+++ hr-user/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO tb_user (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO tb_user (name, email, password) VALUES ('Jacson Alves', 'jacsonp.alves@gmail.com', '$2a$10$HyEomcYbUMpgVnvlE61RCutEj6UAIuRBYQ9x5yxeo3ASwrSB4AVC6');
+
+
+
+
+INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (role_name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
